2e286791dbd711b11c700a03217d24028e9aad04
sammagid <samptmagid@gmail.com>
2025-07-29 14:51:34 -0700

    messing with proxy settings

--- selenium/pythonGPT.py
+++ selenium/pythonGPT.py
@@ -9,24 +9,8 @@ from selenium.webdriver.support import expected_conditions as EC
 # local imports
 import config
 
-# proxy details
-proxy_host = config.PROXY_HOST
-proxy_port = config.PROXY_PORT
-proxy_user = config.PROXY_USER
-proxy_pass = config.PROXY_PASS
-
-# set up Firefox options
-options = Options()
-options.set_preference("network.proxy.type", 1)
-options.set_preference("network.proxy.socks", proxy_host)
-options.set_preference("network.proxy.socks_port", proxy_port)
-options.set_preference("network.proxy.socks_version", 5)
-options.set_preference("network.proxy.socks_username", proxy_user)
-options.set_preference("network.proxy.socks_password", proxy_pass)
-options.set_preference("signon.autologin.proxy", True)
-
 # start Firefox
-driver = webdriver.Firefox(options=options)
+driver = webdriver.Firefox()
 
 # changing the property of the navigator value for webdriver to undefined 
 driver.execute_script("Object.defineProperty(navigator, 'webdriver', {get: () => undefined})")
